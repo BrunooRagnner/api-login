26e41c46ce8182efbce5f1808401f47145e60fb2
Bruno Ragner <141203018+BrunooRagnner@users.noreply.github.com>
2025-06-26 01:22:45 -0300

    Update app.py
     final do códig0

--- app.py
+++ app.py
@@ -131,8 +131,10 @@ def protected():
         "telefone": user.telefone
     }), 200
 
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
